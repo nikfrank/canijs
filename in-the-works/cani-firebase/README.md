a59f4b1d2a2dc2c11a0c54554055bf3fd649f324
nik frank <nikbfrank@gmail.com>
2015-09-20 00:04:24 +0300

    firebase api examples rough

--- in-the-works/cani-firebase/README.md
+++ in-the-works/cani-firebase/README.md
@@ -17,3 +17,23 @@ set an initOn in the cani firebase config
 I've yet to implement any of the read write or watch
 
 that's next nu
+
+---
+
+use keys to link data
+
+keep the actual json flattish
+
+---
+
+use indices {blah1:true, hmmrawr:true,..} to query contains?
+
+---
+
+remember to whitelist the public domain!
+
+---
+
+https://github.com/firebase/firebase-token-generator-node
+
+for lambdauthing
--- in-the-works/cani-firebase/cani-firebase.js
+++ in-the-works/cani-firebase/cani-firebase.js
@@ -1,31 +1,211 @@
 Cani.firebase = (function(firebase){
     var ref;
+    var baseurl;
 
     // put schemas and containers into frebase online
     // futz with the fb profile package and max it out
 
     Cani.core.on('config: firebase', function(conf){
+	firebase.baseurl = baseurl = conf.firebase.url;
 	ref = new Firebase(conf.firebase.url);
 	firebase.ref = ref;
 
-
+// point this to firebase.auth.fb.onLogin(fn)
+// https://www.firebase.com/docs/web/guide/login/facebook.html
 	if(conf.firebase.initOn.indexOf('fb: login')>-1){
 	    Cani.core.confirm('fb: login').then(function(fb){
-		console.log(fb);
 		// login firebase
-
 		ref.authWithOAuthPopup("facebook", function(error, authData) {
+		    if(error)
+			console.log("Login Failed!", error);
+		    else
+			Cani.core.affirm('firebase: fb-login', authData);
+		});
+	    });
+	}
+    });
+
+    firebase.write = function(path, data){
+	
+    };
+
+    firebase.read = function(path, eventType){
+	// use eventType = value for reads
+	// child_added for array pushes
+	// child changed/removed for collection ops
+	// child_moved for ordered collections
+
+	// value triggered last (does this matter?)
+
+
+	// once for .once? yep... gonna need that
+//	var ref = new Firebase("https://docs.firebaseio.com/web/org/users/mchen/groups/alpha");
+//	ref.once('value', function(snap) {
+//	    var result = snap.val() === null? 'is not' : 'is';
+//	    console.log('Mary ' + result + ' a member of alpha group');
+//	});
+
+
+	var def = Q.defer();
+	(new Firebase(baseurl+'/'+path)).on(eventType||'value', function(snapshot){
+	    def.resolve(snapshot.val())
+	}, function(err){
+	    def.reject(err);
+	});
+
+	return def.promise;
+
+	// to kill this callback, save a ref to the ref and .off the eventType
+	// aka fix this code for to no memory leak
+    };
+
+    firebase.query = function(path, orderBy){
+	// can only order each path-query once at a time
+	// keep an index of active queries
+	var ref = new Firebase("https://dinosaur-facts.firebaseio.com/dinosaurs");
+
+//	ref.orderByKey().on("child_added", function(snapshot){
+
+// limitTo First/Last, orderByValue
+
+//	ref.orderByChild("weight").limitToLast(2).on("child_added", function(snapshot){
+//	ref.orderByChild("height").limitToFirst(2).on("child_added", function(snapshot) {
+
+//	var scoresRef = new Firebase("https://dinosaur-facts.firebaseio.com/scores");
+//	scoresRef.orderByValue().limitToLast(3).on("value", function(snapshot) {
+
+// startAt(), endAt(), and equalTo()
+
+//	ref.orderByChild("height").startAt(3).on("child_added", function(snapshot) {
+//	ref.orderByKey().endAt("pterodactyl").on("child_added", function(snapshot) {
+//	ref.orderByKey().startAt("b").endAt("b~").on("child_added", function(snapshot) {
+// ~ is the last "regular ascii char"
+//	ref.orderByChild("height").equalTo(25).on("child_added", function(snapshot) {
+
+
+
+	ref.orderByChild("height").on("child_added", function(snapshot){
+	    console.log(snapshot.key() + " was " + snapshot.val().height + " meters tall");
+	});
+    };
+    
+
+    firebase.transact = function(path, transactionFn){
+	var upvotesRef = new Firebase('https://docs-examples.firebaseio.com/android/saving-data/fireblog/posts/-JRHTHaIs-jNPLXOQivY/upvotes');
+	upvotesRef.transaction(function (current_value) {
+	    return (current_value || 0) + 1;
+	});
+    };
+
+    // should have a bind/unbind
+    // look into zonejs for this type of thing.
+
+
+
+// auth stuff, should be split into sep modules?
+
+    firebase.auth = {
+	email:{
+//https://www.firebase.com/docs/web/guide/login/password.html
+//        ".read": "auth !== null && auth.provider === 'password'"
+	    createUser:function(email, password){
+//		var ref = new Firebase("https://<YOUR-FIREBASE-APP>.firebaseio.com");
+		ref.createUser({
+		    email    : "bobtony@firebase.com",
+		    password : "correcthorsebatterystaple"
+		}, function(error, userData) {
+		    if (error) {
+			console.log("Error creating user:", error);
+		    } else {
+			console.log("Successfully created user account with uid:", userData.uid);
+		    }
+		});
+	    },
+	    login:function(email, password){
+//		var ref = new Firebase("https://<YOUR-FIREBASE-APP>.firebaseio.com");
+		ref.authWithPassword({
+		    email    : "bobtony@firebase.com",
+		    password : "correcthorsebatterystaple"
+		}, function(error, authData) {
 		    if (error) {
 			console.log("Login Failed!", error);
 		    } else {
 			console.log("Authenticated successfully with payload:", authData);
 		    }
+		}, { // do this in an option?
+		    remember: "sessionOnly"
 		});
+	    },
 
-	    });
+	    changeEmail:function(oldEmail, nuEmail, password){
+//		var ref = new Firebase("https://<YOUR-FIREBASE-APP>.firebaseio.com");
+		ref.changeEmail({
+		    oldEmail : "bobtony@firebase.com",
+		    newEmail : "bobtony@google.com",
+		    password : "correcthorsebatterystaple"
+		}, function(error) {
+		    if (error === null) {
+			console.log("Email changed successfully");
+		    } else {
+			console.log("Error changing email:", error);
+		    }
+		});
+	    },
+	    changePassword:function(email, oldPassword, nuPassword){
+//		var ref = new Firebase("https://<YOUR-FIREBASE-APP>.firebaseio.com");
+		ref.changePassword({
+		    email       : "bobtony@firebase.com",
+		    oldPassword : "correcthorsebatterystaple",
+		    newPassword : "neatsupersecurenewpassword"
+		}, function(error) {
+		    if (error === null) {
+			console.log("Password changed successfully");
+		    } else {
+			console.log("Error changing password:", error);
+		    }
+		});
+	    },
+	    sendPasswordReset:function(email){
+//		var ref = new Firebase("https://<YOUR-FIREBASE-APP>.firebaseio.com");
+		ref.resetPassword({
+		    email : "bobtony@firebase.com"
+		}, function(error) {
+		    if (error === null) {
+			console.log("Password reset email sent successfully");
+		    } else {
+			console.log("Error sending password reset email:", error);
+		    }
+		});
+	    },
+	    deleteAccount:function(email, password){
+//		var ref = new Firebase("https://<YOUR-FIREBASE-APP>.firebaseio.com");
+		ref.removeUser({
+		    email    : "bobtony@firebase.com",
+		    password : "correcthorsebatterystaple"
+		}, function(error) {
+		    if (error === null) {
+			console.log("User removed successfully");
+		    } else {
+			console.log("Error removing user:", error);
+		    }
+		});
+	    }
 	}
-    });
-    
+    };
+
+
+    firebase.connection = function(){
+//https://www.firebase.com/docs/web/guide/offline-capabilities.html
+	var connectedRef = new Firebase("https://<YOUR-FIREBASE-APP>.firebaseio.com/.info/connected");
+	connectedRef.on("value", function(snap) {
+	    if (snap.val() === true) {
+		alert("connected");
+	    } else {
+		alert("not connected");
+	    }
+	});
+    };
+
 
     return firebase;
 
