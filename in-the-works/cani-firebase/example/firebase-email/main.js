969f05c0600c9d30fe26e8093b87220bc5cbc374
nik frank <nikbfrank@gmail.com>
2015-09-29 19:18:00 +0300

    firebase demo login logout working

--- in-the-works/cani-firebase/example/firebase-email/main.js
+++ in-the-works/cani-firebase/example/firebase-email/main.js
@@ -2,22 +2,35 @@
 
 angular.module('firebase-email', ['ui.router']).run(function($q){ window.Q = $q; })
 
+.run(function($location, $q){
+    window.Q = $q;
+    $location.path('/login');
+})
+
 .config(function($stateProvider, $urlRouterProvider){
-    $urlRouterProvider.otherwise("/");
+    $urlRouterProvider.otherwise("/login");
 
     $stateProvider.state('login', {
-	url: "/",
+	url: "/login",
 	templateUrl: "views/login.html",
-	controller: 'LoginCtrl as login'
+	controller: 'LoginCtrl as login',
+	resolve:{
+	    CaniNoAuth:function($state){
+		Cani.core.confirm('firebase-auth: email-login').then(function(authData){
+		    $state.go('main'); // logged in from before!
+		});
+		// otherwise no login from before, load the login page
+		return Cani.core.confirm('firebase-auth: no-persisted-login');
+	    }
+	}
 
     }).state('main', {
 	url: "/main",
 	templateUrl: "views/main.html",
-	controller: 'MainCtrl as ctrl',
+	controller: 'MainCtrl as main',
 	resolve:{
-	    CaniDep:function(auth){ // this DI is what triggers login on refresh from #/main
-		console.log('confirming...');
-		return Cani.core.confirm(['firebase-auth: email-login']);
+	    CaniAuth:function(auth){ // this DI is what triggers login on refresh from #/main
+		return Cani.core.confirm('firebase-auth: email-login');
 	    }
 	}
     });
@@ -25,25 +38,22 @@ angular.module('firebase-email', ['ui.router']).run(function($q){ window.Q = $q;
 
 .service('auth', function($state){
     var that = this;
+
     this.confirmLogin = Cani.core.confirm('firebase-auth: email-login')
-	.then(console.log.bind(console))
-	.then(function(){
-	    $state.go('main');
-	})
-    // then forward to the main state
+	.then(function(authData){ return that.authData = authData;})
+    // save authData on login to the auth service in case we need it somewhere
+	.then(function(){ $state.go('main');});
+    // go to /main on login
 })
 
 .controller('LoginCtrl', function(auth, $state){ // this DI is what triggers login from new page load
-    auth.confirmLogin.then(function(cogId){
-	$state.go('main');
-    });
     this.login = function(email, password){
 	// login to firebase
-	Cani.firebase.auth.email.login(email, password)
+	Cani.firebase.auth.email.login(email, password, 'default')
 	    .then(function(res){
 		console.log('logged in', res);
 	    }, function(err){
-		console.log('login failed', err);
+		alert('login failed\n'+err); // the firebase err obj toString()s nicely!
 	    });
     };
 
@@ -58,19 +68,28 @@ angular.module('firebase-email', ['ui.router']).run(function($q){ window.Q = $q;
     };
 })
 
-.controller('MainCtrl', function(){
+.controller('MainCtrl', function($scope, CaniAuth){
     var that = this;
-
-
-    console.log(Cani.firebase);
-
+    this.user = CaniAuth.password.email;
     Cani.core.confirm('firebase').then(function(firebase){
-	console.log(firebase);
 
-	// expose auth functions TO A SCOPE OBJ
-	window.login = firebase.auth.login;
-	window.signup = firebase.auth.createUser;
-	window.sendNuPassword = firebase.auth.sendPasswordReset;
+	$scope.login = firebase.auth.login;
+	$scope.signup = firebase.auth.createUser;
+	$scope.sendNuPassword = firebase.auth.sendPasswordReset;
+
+	$scope.changePassword = function(){
+	    // show form to ask for old credentials and new credentials
+	    firebase.auth.changePassword();
+	};
+
+	$scope.changeEmail = function(){
+	    // show form to ask for old credentials and new credentials
+	    firebase.auth.changeEmail();
+	};
+
+	$scope.logout = function(){
+	    firebase.auth.logout().then(location.reload.bind(location));   
+	};
     });
 });
 
